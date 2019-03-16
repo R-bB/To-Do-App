08a84ce16734b06df639b0e08db25837b28666a3
r-b_b <r.boehme@outlook.com>
2019-03-16 15:27:51 -0500

    Added Google, Facebook, Github, and Email sign in. Changed Header Height.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import firebase from "firebase";
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+const config = {
+  apiKey: "AIzaSyBamdjooQEKP0OBoiTMme9EglbsiUTYawk",
+  authDomain: "to-do-app-684f2.firebaseapp.com",
+}
+firebase.initializeApp(config);
 
 class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    }
+    )
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello Launchcode!
-          </p>
-        </header>
+      <header className="App-header">
+        <h1>To-Do</h1>
+      </header>
+      {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h3>Welcome {firebase.auth().currentUser.displayName}</h3>
+          </span>
+        ) : (
+          <span>
+            <h3>Please sign in</h3>
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </span>
+        )
+      }
       </div>
     );
   }
 }
 
 export default App;
+
